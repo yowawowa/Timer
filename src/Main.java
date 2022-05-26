5b953d8931b82ba68a39e3f313101b01f0663d4c
Vladislav Kostin <92290777+yowawowa@users.noreply.github.com>
2022-05-26 10:39:44 +0500

    total rework of timer structure and ideology

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        new MyNewFrame();
+    public static void main(String[] args) {
+        new Pomodoro();
+
     }
 }
